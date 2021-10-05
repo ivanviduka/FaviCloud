5f3e55f11ac3558fb0203e90cf0eea8424958b2a
ivanviduka <viduka.ivan@gmail.com>
2021-10-05 14:27:39 +0200

    Removed toJson from API methods

--- FaviCloud.loc/routes/api.php
+++ FaviCloud.loc/routes/api.php
@@ -20,16 +20,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('files/{id}', function ($id) {
-    $files = File::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+    $files = File::where('id', $id)->get();
     return response($files, 200);
 });
 Route::get('files', function () {
-    $files = File::all()->toJson(JSON_PRETTY_PRINT);
+    $files = File::all();
     return response($files, 200);
 });
 
 Route::get('user-files/{user_id}', function ($user_id) {
-    $files = File::where('user_id', $user_id)->get()->toJson(JSON_PRETTY_PRINT);
+    $files = File::where('user_id', $user_id)->get();
     return response($files, 200);
 });
 
